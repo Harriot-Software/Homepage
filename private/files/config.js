7855b4b4c7ef84aa0b59f6ba70ed1ff7bc83bbe6
Zecuel <Kuikka87@gmai.com>
2020-07-24 13:29:08 +0300

    Updated directory layout

---
+++ private/files/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    ports: {
+        http: 80,
+        ssl: 8082,
+        ssl_dev: 8083
+    },
+
+    mailgun: {
+        domain: "sandboxd10b5af1434f460baebb0347bb50528b.mailgun.org",
+        key: "key-d05cba9e9ee63f80089d679c4dcc1fc2"
+    }
+
+};
